f1f1a944c0eb2794cd2df62ce9f527317a25a86d
Paul Thiebauth <paulmthiebauth@gmail.com>
2015-04-01 16:53:00 -0400

    Updated review form to include delete comment button.

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,13 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
-  resources :vendors
+  resources :vendors, only: [:index, :show, :edit, :update]
 
   resources :vendors do
     resources :locations
   end
 
-  resources :vendors do
+  resources :vendors, only: [:index, :show] do
     resources :reviews
   end
 
