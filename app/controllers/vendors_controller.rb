c407b88a868ae74c987ccb7452be8afa22384ff6
iEv0lv3 <jordan.wallace.williams@gmail.com>
2015-04-01 17:23:31 -0400

    Updated Controllers and Vendor Index -Added form to submit vendor -Added links through to vendor details -Set up vendor index loop to gather vendors

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -1,11 +1,12 @@
 class VendorsController < ApplicationController
   def index
     @vendors = Vendor.all
+    @new_vendor = Vendor.new
   end
 
   def show
     @vendor = Vendor.find(params[:id])
-    @vendor.reviews
+    @reviews = @vendor.reviews
   end
 
   def new
@@ -13,5 +14,19 @@ class VendorsController < ApplicationController
   end
 
   def create
+    @vendor = Vendor.create(vendor_params)
+    if @vendor.save
+      flash[:notice] = "Vendor added. Pending review."
+      redirect_to vendors_path
+    else
+      flash[:notice] = "Invalid entry"
+      render :back
+    end
   end
+
+  protected
+  def vendor_params
+    params.require(:vendor).permit(:vendor_name)
+  end
+
 end
