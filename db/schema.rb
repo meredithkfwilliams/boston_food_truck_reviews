040405d2cf86c7cf173e258a8c88651c1ddb5af6
iEv0lv3 <jordan.wallace.williams@gmail.com>
2015-04-01 11:00:12 -0400

    Resolving user type default value conflict

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150331174957) do
+ActiveRecord::Schema.define(version: 20150331180457) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,7 +50,7 @@ ActiveRecord::Schema.define(version: 20150331174957) do
     t.datetime "last_sign_in_at"
     t.inet     "current_sign_in_ip"
     t.inet     "last_sign_in_ip"
-    t.string   "user_type",              default: "member", null: false
+    t.string   "user_type",              default: "Member", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
