866cc2c773ddd49ed6f98b80f6077e98ba6073ca
Paul Thiebauth <paulmthiebauth@gmail.com>
2015-04-07 16:21:09 -0400

    Completed missing Acceptance Criteria for feature tests.

--- spec/features/member_reviews_vendor_spec.rb
+++ spec/features/member_reviews_vendor_spec.rb
@@ -5,8 +5,17 @@ feature 'member posts vendor review', %{
   I want to be able to leave a review for a specific truck
   So that I can share my experience with other users
 } do
-
   # Acceptance Criteria
+  # * If I'm signed in, I should be able to rate/review vendors
+  # * If I don't fill out the review form properly
+  # * I get an error telling me what the issues are
+
+  # * If I create a review for a vendor
+  # * I should be able to delete it
+
+  # * If I'm not signed in, I shouldn't be able to rate/review vendors
+  # * When I view a vendors details
+  # * I should not be able to view the "new review" form
 
   scenario 'member posts a new review for a specific vendor' do
     review_vendor
--- spec/features/member_votes_review_spec.rb
+++ spec/features/member_votes_review_spec.rb
@@ -5,6 +5,16 @@ feature 'member votes on a review', %{
   I want to be able to leave an up or down vote on each review
   So that I can share my opinion with other users
 } do
+  # Acceptance Criteria
+  # * If I'm signed in, I should be able to up/down vote a review
+  # * If I up or down/vote a review
+  # * I should see the score update on the page
+
+  # * If I up or down/vote a review
+  # * I should be able to modify my vote
+
+  # * If I up or down/vote a review
+  # * I should not be able to submit the same vote a second time
 
   scenario 'member posts up-vote to a review' do
     review_and_vote
