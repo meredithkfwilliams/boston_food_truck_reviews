86a9a4b620f70f8cbd1a41d8dff82728399fdae7
Paul Thiebauth <paulmthiebauth@gmail.com>
2015-04-08 15:10:14 -0400

    The ACTUAL Final PR 34 cleanup.  Added :destroy to vendor routing to pass final test.

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,11 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
-  resources :vendors, only: [:index, :show, :edit, :update, :create] do
+  resources :vendors, only: [:index, :show, :edit, :update, :create, :destroy] do
     resources :reviews do
       resources :votes, only: [:create]
     end
-
   end
 
   resources :locations
-
 end
