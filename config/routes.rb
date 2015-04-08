4002f3824be4dcbbd53e9ceb52d095cbd601555b
Paul Thiebauth <paulmthiebauth@gmail.com>
2015-04-08 15:17:19 -0400

    Fixed line that was too long.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'homes#index'
   devise_for :users
 
-  resources :vendors, only: [:index, :show, :edit, :update, :create, :destroy] do
+  resources :vendors,
+    only: [:index, :show, :edit, :update, :create, :destroy] do
     resources :reviews do
       resources :votes, only: [:create]
     end
