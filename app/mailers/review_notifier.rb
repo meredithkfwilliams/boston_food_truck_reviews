5909dc7df95b7942f6a24065c9e760d070d7ba1a
Paul Thiebauth <paulmthiebauth@gmail.com>
2015-04-09 17:31:48 -0400

    Cleaned up minor issues for Travis.

--- app/mailers/review_notifier.rb
+++ app/mailers/review_notifier.rb
@@ -2,11 +2,10 @@ class ReviewNotifier < ApplicationMailer
   default from: "reviews@example.com"
   def new_review(review)
     @review = review
-      if !@review.vendor.owner_id.nil?
-        binding.pry
-        mail(
+    if !@review.vendor.owner_id.nil?
+      mail(
         to: User.find(@review.vendor.owner_id).email,
         subject: "New Review for #{@review.vendor.vendor_name}")
-      end
+    end
   end
 end
--- spec/mailers/previews/review_notifier_preview.rb
+++ spec/mailers/previews/review_notifier_preview.rb
@@ -1,4 +1,3 @@
 # Preview all emails at http://localhost:3000/rails/mailers/review_notifier
 class ReviewNotifierPreview < ActionMailer::Preview
-
 end
