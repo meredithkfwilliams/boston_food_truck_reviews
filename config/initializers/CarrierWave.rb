c73a3433f40e71a10ddb98d89edbb8b60441a62e
Meredith Fuhrman <meredith.fuhrman@gmail.com>
2015-04-10 15:12:45 -0400

    Update for Heroku

--- config/initializers/CarrierWave.rb
+++ config/initializers/CarrierWave.rb
@@ -1,9 +1,11 @@
 CarrierWave.configure do |config|
+  config.root = Rails.root.join('tmp')
+  config.cache_dir = 'carrierwave'
+
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"] || "",
     aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"] || ""
     }
   config.fog_directory  = ENV["S3_BUCKET"]
-
 end
