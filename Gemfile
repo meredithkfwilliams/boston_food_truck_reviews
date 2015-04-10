7b8ab6b61418d98ff318b22683c79a3ec41f5619
Meredith Fuhrman <meredith.fuhrman@gmail.com>
2015-04-09 21:45:04 -0400

    Pushing up PR #39 for review

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'foundation-rails', "5.5.1.0"
 gem 'kaminari'
 gem 'rails_12factor', group: :production
 gem 'carrierwave'
+gem 'carrierwave_direct'
 gem 'fog'
 gem 'fog-aws'
 gem 'mini_magick'
--- config/initializers/CarrierWave.rb
+++ config/initializers/CarrierWave.rb
@@ -6,3 +6,10 @@ CarrierWave.configure do |config|
   }
   config.fog_directory  = ENV["S3_BUCKET"]
 end
+
+if Rails.env.test?
+    CarrierWave.configure do |config|
+      config.storage = :file
+      config.enable_processing = false
+    end
+end
