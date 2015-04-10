056fa49bec745169d3eec6712e2fd482a6d96c1b
Meredith Fuhrman <meredith.fuhrman@gmail.com>
2015-04-10 12:42:01 -0400

    Updating Carrierwave to clear Travis failures

--- config/initializers/CarrierWave.rb
+++ config/initializers/CarrierWave.rb
@@ -1,11 +1,9 @@
 CarrierWave.configure do |config|
   if Rails.env.test?
-    CarrierWave.configure do |config|
       config.storage = :file
       config.enable_processing = false
-    end
   end
-  
+
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
