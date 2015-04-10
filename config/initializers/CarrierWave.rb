a55047c5ec546d567a4b362bfbe04425f1e0f7ba
Meredith Fuhrman <meredith.fuhrman@gmail.com>
2015-04-10 12:53:09 -0400

    One last change to get Travis build to pass

--- config/initializers/CarrierWave.rb
+++ config/initializers/CarrierWave.rb
@@ -1,13 +1,8 @@
 CarrierWave.configure do |config|
-  if Rails.env.test?
-      config.storage = :file
-      config.enable_processing = false
-  end
-
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"],
-    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"]
+    aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"] || "",
+    aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"] || ""
     }
   config.fog_directory  = ENV["S3_BUCKET"]
 
